bc9421b7c41475bb69fd64e945b7e1f471f5343a
宇宙伟 <2117008741@qq.com>
2024-07-15 23:51:39 +0800

    1

--- palmx-benchmark/src/main/java/me/wei/benchmark/GreetingService.java
+++
@@ -1,9 +0,0 @@
-package me.wei.benchmark;
-
-import me.xuqu.palmx.spring.PalmxService;
-
-// 定义服务接口
-public interface GreetingService {
-    String sayHello(String name);
-}
-
--- palmx-benchmark/src/test/java/me/wei/benchmark/PalmxBenchmark.java
+++ palmx-benchmark/src/test/java/me/wei/benchmark/PalmxBenchmark.java
@@ -1,41 +1,58 @@
 package me.wei.benchmark;
 
+import me.wei.ConsumerRun;
+import me.wei.service.PalmxService;
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-import javax.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
-@BenchmarkMode(Mode.Throughput)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
-@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 3, time = 1)
+@Measurement(iterations = 10, time = 1)
+@Threads(1)
+@Fork(1)
+@State(value = Scope.Benchmark)
+@OutputTimeUnit(TimeUnit.SECONDS)
 public class PalmxBenchmark {
 
-    @Resource
-    private GreetingClient client;
 
-    @Setup(Level.Trial)
-    public void setup() {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        client = context.getBean(GreetingClient.class);
+    @Param(value = {"10", "100", "1000", "10000", "100000"})
+    private int param;
+
+    private ConfigurableApplicationContext context;
+    private PalmxService palmxService;
+
+    @Setup
+    public void init() {
+        // 这里的WebApplication.class是项目里的spring boot启动类
+        context = SpringApplication.run(ConsumerRun.class);
+        // 获取需要测试的bean
+        this.palmxService = (PalmxService) context.getBean("palmxService");
+    }
+
+    @TearDown
+    public void down() {
+        context.close();
     }
 
     @Benchmark
-    public void testRPCPerformance() {
-        client.callSayHello("World");
+    public void invoke() {
+        palmxService.invoke();
     }
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
                 .include(PalmxBenchmark.class.getSimpleName())
-                .forks(1)
-                .build();
-
+                .result(LocalDateTime.now().getMinute() + "-PalmxBenchmark.json")
+                .resultFormat(ResultFormatType.JSON).build();
         new Runner(opt).run();
     }
 }
