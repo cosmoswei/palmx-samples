7d2c75669fcfdd160c82fe57713c0bf80835289a
宇宙伟 <2117008741@qq.com>
2024-07-23 16:23:33 +0800

    1

--- palmx-provider/src/main/java/me/wei/service/TestServiceImpl.java
+++ palmx-provider/src/main/java/me/wei/service/TestServiceImpl.java
@@ -1,11 +1,12 @@
 package me.wei.service;
 
+import me.xuqu.palmx.common.FlowControlType;
 import me.xuqu.palmx.spring.PalmxService;
 
 import java.util.concurrent.TimeUnit;
 
-@PalmxService
-public class DemoServiceImpl implements DemoService {
+@PalmxService(flowControlLimitCount = 100000, flowControlLimitType = FlowControlType.ADAPTIVE)
+public class TestServiceImpl implements DemoService {
 
     @Override
     public String demoSleepSecond(long l) {
@@ -14,6 +15,6 @@ public class DemoServiceImpl implements DemoService {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        return "DemoServiceImpl success" + System.currentTimeMillis() % 10000;
+        return "TestServiceImpl success" + System.currentTimeMillis() % 10000;
     }
 }
